55ed81b0bf2ddb866700f91d9547bc872de1308c
xoreo <mattnappo@gmail.com>
2018-09-12 12:00:46 -0400

    Added trycatch handling

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 var Fraction = algebra.Fraction;
 var Expression = algebra.Expression;
 var Equation = algebra.Equation;
-$("#answer_output").hide();
 
 function execute() {
     var input_form = document.getElementById("input_form");
@@ -21,26 +20,34 @@ var SOLVER = SOLVER || (function() {
             
             var raw_equation = document.getElementById("equation").value;
             solve_for = document.getElementById("solve_for").value;
-
-            if (raw_equation.indexOf('=') > -1) {
-                raw_equation = raw_equation.split("=");
-                var left_side = algebra.parse(raw_equation[0]);
-                var right_side = algebra.parse(raw_equation[1]);
-                equation = new Equation(left_side, right_side);
-                answer = equation.solveFor(solve_for);
-            } else {
-                equation = algebra.parse(raw_equation);
-                console.log("expression: " + equation);
-                answer = equation;
+            try {
+                if (raw_equation.indexOf('=') > -1) {
+                    raw_equation = raw_equation.split("=");
+                    var left_side = algebra.parse(raw_equation[0]);
+                    var right_side = algebra.parse(raw_equation[1]);
+                    equation = new Equation(left_side, right_side);
+                    answer = equation.solveFor(solve_for);
+                } else {
+                    equation = algebra.parse(raw_equation);
+                    console.log("expression: " + equation);
+                    answer = equation;
+                }
+                this.solve();
+            } catch {
+                this.fail();
             }
-            this.solve();
+            
         },
         solve : function() {
             $("#answer_output").show();
-            
             document.getElementById("equation_header").innerHTML = equation;
             document.getElementById("equation_answer").innerHTML = solve_for + " = " + answer;
     
+        },
+        fail : function() {
+            $("#answer_output").show();
+            document.getElementById("equation_header").innerHTML = "Oops!";
+            document.getElementById("equation_answer").innerHTML = "It looks like we couldn't solve that one!";
         }
     };
 
